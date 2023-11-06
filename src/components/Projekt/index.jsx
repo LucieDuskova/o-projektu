e70359e50fc7b0b39ce9187c7b3e0ad9ed1962bd
Linda Hrdinová <hrlin@seznam.cz>
2023-11-06 15:35:57 +0100

    Přidání vlastního návrhu projektu

--- src/components/Projekt/index.jsx
+++ src/components/Projekt/index.jsx
@@ -3,7 +3,10 @@ import './style.css';
 export const Projekt = () => {
   return (
     <main>
-      <img src="https://media.istockphoto.com/id/172803481/cs/fotografie/pozad%C3%AD-p%C5%99ikr%C3%BDvky.jpg?s=1024x1024&w=is&k=20&c=C-o17l9J3XyVwL4BEFT3SwbJ-oUfxYWTysZ_jsz0H84=" alt="herní plán Patchwork" />
+      <img
+        src="./components/Projekt/img/Patchwork.png"
+        alt="herní plán Patchwork"
+      />
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum ducimus
         maxime alias inventore laboriosam ex, in expedita sit, laborum quia
